f293a93d15b727a72ba249bb3feca7fd09654009
reginaldo2307 <reginaldo1982@hotmail.com>
2022-01-04 10:09:16 -0300

    Primeiro commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    	DB::table('units')->insert([
+    		'name' => '100',
+    		'id_owner' => '1'
+    	]);
+
+    	DB::table('units')->insert([
+    		'name' => '101',
+    		'id_owner' => '1'
+    	]);
+
+    	DB::table('units')->insert([
+    		'name' => '200',
+    		'id_owner' => '0'
+    	]);
+
+    	DB::table('units')->insert([
+    		'name' => '201',
+    		'id_owner' => '0'
+    	]);
+
+    	DB::table('areas')->insert([
+    		'allowed' => '1',
+    		'title' => 'Academia',
+    		'cover' => 'gym.jpg',
+    		'days' => '1,2,4,5',
+    		'start_time' => '06:00:00',
+    		'end_time' => '22:00:00'
+    	]);
+
+    	DB::table('areas')->insert([
+    		'allowed' => '1',
+    		'title' => 'Piscina',
+    		'cover' => 'pool.jpg',
+    		'days' => '1,2,3,4,5',
+    		'start_time' => '07:00:00',
+    		'end_time' => '22:00:00'
+    	]);
+
+    	DB::table('areas')->insert([
+    		'allowed' => '1',
+    		'title' => 'Churrasqueira',
+    		'cover' => 'barbecue.jpg',
+    		'days' => '4,5,6',
+    		'start_time' => '09:00:00',
+    		'end_time' => '23:00:00'
+    	]);
+
+    	DB::table('walls')->insert([
+    		'title' => 'Título de Aviso de Teste',
+    		'body' => 'Teste teste teste',
+    		'datecreated' => '2021-12-20 22:00:00',
+    	]);
+
+    	DB::table('walls')->insert([
+    		'title' => 'Título de Alerta Geral',
+    		'body' => 'Teste teste teste Geral',
+    		'datecreated' => '2021-12-20 23:00:00',
+    	]);
+    }
+}
